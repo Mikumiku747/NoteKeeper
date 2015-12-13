5ab18b72e8a80da2af7338b6a5d5492ef55cd06a
Daniel Selmes <daniel@lenovo-debian>
2015-12-13 18:05:40 +1100

    Initial commit This time I'm going to use autoconf as well as make and git, so now the workflow is awkward and s=convoluted as hell, but everything else should be faster and no mistakes can be made (hopefully). Currently, the source file does nothing except init gtk and return 0.

---
+++ README.md
@@ -0,0 +1,5 @@
+Notekeeper
+==========
+
+A notekeeping program written in C, using GTK+. Similar to Microsoft 
+OneNote.
---
+++ src/notekeeper.c
@@ -0,0 +1,31 @@
+/*  notekeeper.c - A program for taking notes, similar to Microsoft's
+	OneNote. 
+    Copyright (C) 2015  Daniel Selmes
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* Library Includes */
+#include <gtk/gtk.h>
+
+/* Source Includes */
+
+/* Main */
+int main(int argc, char *argv)
+{
+	//Initialise GTK
+	gtk_init(&argc, &argv);
+	
+	return 0;
+}
